215cb0582936a58c4f1b685e5e8993e09cb7faac
Ochaworldwide <lazarusgabriel000@gmail.com>
2022-08-24 13:02:49 +0100

    update

--- Assets/cal.js
+++ Assets/cal.js
@@ -1,69 +1,42 @@
 function clicker(a){
  var displayer=document.getElementById("disp");
- displayer.value+=a;
-
-
-  
-}
-function doublez(){
-    var displayer=document.getElementById("disp");
-    displayer.value+="00";
-    // displayer.innerText+="*"
-    
+ if(displayer.value.includes("=")){
+    displayer.value=a;
+ }
+ else{
+    displayer.value+=a;
+ }
+ 
 }
 
-function star(){
-    var displayer=document.getElementById("disp");
-    displayer.value+="*";
-    // displayer.innerText+="*"
-    
-}
-function sub(){
-    var displayer=document.getElementById("disp");
-    displayer.value+="-";
-    // displayer.innerText+="-"
-    
-}
-function add(){
-    var displayer=document.getElementById("disp");
-    displayer.value+="+";
-    // displayer.innerText+="+"
-    
-}
 function equal(){
     var displayer=document.getElementById("disp");
     displayer.value+= "  =   " + eval(displayer.value);
-    // displayer.innerText=eval()
-    
-    
+       
 }
-function dot(){
-    var displayer=document.getElementById("disp");
-    displayer.value+=".";
-    // displayer.innerText+="."
-    
-}
-function mod(){
-    var displayer=document.getElementById("disp");
-    displayer.value+="%";
-    // displayer.innerText+="."
-    
-}
-function divide(){
-    var displayer=document.getElementById("disp");
-    displayer.value+="/";
-    // displayer.innerText+="."
-    
-}
-function sqroot(){
-    var displayer=document.getElementById("disp");
-    displayer.value+="^";
-    // displayer.innerText+="."
-    
-}
+
 function clearer(){
     var displayer=document.getElementById("disp").value="";
-    // displayer.value="";
-    // displayer.innerText+="."
+       
+}
+
+function Off(){
+    if(document.getElementById("disp").style.visibility=="hidden"){
+        document.getElementById("disp").style.visibility="visible";
+        document.getElementById("disp").value="";
+    }
+    else{
+        document.getElementById("disp").style.visibility="hidden";
+        document.getElementById("disp").value="";
+    }
     
 }
+
+function switchTheme() {
+    let theme = document.getElementById("theme");
+    if (theme.getAttribute("href") == "Assets/light.css") {
+      theme.href = "Assets/dark.css";
+    } else {
+      theme.href = "Assets/light.css";
+    }
+  }
